cfce8ec07709b1e85c26eef21c875f27c8f37c49
yoestudiocr <yoestudiocr@gmail.com>
2026-07-16 01:52:15 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,38 @@
-const CACHE_NAME = "tina-v2";
-const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "tina-v1.0.0";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./css/main.css",
+  "./js/app.js",
+  "./js/storage.js",
+  "./js/utils.js",
+  "./js/insights.js",
+  "./js/report.js",
+  "./assets/logo.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
+
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match("./index.html"))));
+  if(event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+      return response;
+    }).catch(() => caches.match("./index.html")))
+  );
 });
